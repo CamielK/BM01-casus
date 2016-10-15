29bda25aebcdb474fba07032b49313cb87aec74b
Camiel <camielk@live.nl>
2016-10-15 17:32:38 +0200

    Added output and input view

--- Aider/AiderApp/AiderApp/Views/SearchView.cs
+++ Aider/AiderApp/AiderApp/Views/SearchView.cs
@@ -11,18 +11,21 @@ using Newtonsoft.Json.Linq;
 using System.Windows.Forms;
 using System.IO;
 using AiderApp.Controllers;
+using AiderApp.Views;
 
 namespace AiderApp
 {
     public partial class Form1 : Form
     {
-       
+
+        OutputView outputView;
 
         public Form1()
         {
             //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             InitializeComponent();
-            panel1.Visible = false;
+            outputView = new OutputView(this);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,28 +33,11 @@ namespace AiderApp
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private async void button1_Click(object sender, EventArgs e)
         {
+            outputView.Location = this.Location;
+            outputView.Visible = true;
+            this.Visible = false;
 
             SearchController _controller = new SearchController();
 
@@ -60,11 +46,16 @@ namespace AiderApp
             {
                 if (data["law_articles"] != null && data["law_articles"].Any())
                 {
-                    MessageBox.Show(data["law_articles"][1]["article_text"].ToString());
-
+                    //MessageBox.Show(data["law_articles"][1]["article_text"].ToString());
+                    
                     //TODO: dynamicly load json data into results table
-                    label1.Text = data["law_articles"][1]["article_text"].ToString();
-                    panel1.Visible = true;
+                    //for (int i = 0; i < data["law_articles"].Count(); i++)
+                    //{
+
+                    //}
+
+                    outputView.updateOutput(data["law_articles"][1]["article_text"].ToString());
+
                 }
                 else
                 {
@@ -77,26 +68,6 @@ namespace AiderApp
         private void button2_Click(object sender, EventArgs e)
         {
             Close();
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
         }
     }
 }
