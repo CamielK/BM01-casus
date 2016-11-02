86423a76783bdaf9ca731e0e62bff71d1f654f73
Camiel <camielk@live.nl>
2016-11-02 21:48:15 +0100

    Added summary method pseudo code and query builder

--- Aider/DatabaseApi/_class/searchEngine.php
+++ Aider/DatabaseApi/_class/searchEngine.php
@@ -71,6 +71,71 @@ class searchEngine {
     //return a query-focused summary from the documents that match the search string
     public function getSummaryFromLawTexts($searchstring) {
     	
+    	//build query
+    	include_once('queryBuilder.php');
+    	$query_builder = new queryBuilder();
+    	
+    	//split individual query words and get synonyms for each word
+    	$query_array = explode(" ", $searchstring);
+    	foreach ($query_array as $key => $query_word) {
+    	    $query_array[$key] = $query_builder->getSynonym($query_word); 
+    	    
+    	    //TODO: assign a weight to each query word
+    	}
+    	
+    	
+    	
+    	
+    	//get documents
+    	
+    	    //query database for all query words
+    	    //return top 100 most relevant articles
+    	
+    	
+    	
+    	
+    	//create summary
+	//     -	Sentence segmentation:
+    //         o	Pick a set of sentences from multiple documents
+    //     -	Simplify the sentences
+    //         o	(Zajic et al. (2007), Controy et al. (2006), Vanderwende et al. (2007))
+    //         o	Parse sentences, use rules to decide which modifiers to prune. Machine learning methods can be used aswell
+    //     -	Sentence extraction:
+    //         o	log-likelihood ratio (LLR)
+    //         o	Maximal Marginal Relevance (MMR), avoid redundancy
+    //             	(Jaime Cardonell and Jade Goldstein, The Use of MMR, SIGIR-98)
+    //             	Method: Iteratively (greedily) choose the best sentence to insert in the summary so far:
+    //                 •	Relevant: Maximally relevant to the user’s query (high cosine similarity to query)
+    //                 •	Novel: Minimally redundant with the summary so far (low cosine similarity to summary so far) 
+    //             	Stop when desired length of summary is reached
+    //         o	LLR + MMR
+    //             	Combine LLR and MMR to select non-redundant yet informative sentences:
+    //                 1.	Score each sentence based on LLR (including query words)
+    //                 2.	Include the sentence with highest score in the summary
+    //                 3.	Iteratively add into the summary high-scoring sentences that are not redundant with summary so far
+    //     -	Information Ordering
+    //         o	Chronological ordering: order sentences by the date of the document
+    //         o	Coherence:
+    //             	Choose orderings that make neighboring sentences similar (by cosine).
+    //             	Choose orderings in which neighboring sentences discuss the same entity (Barzilay and Lapata 2007)
+    //         o	Topical ordering: learn the ordering of topics in the source documents
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	return json_encode($query_array);
+    	
         //call to database connection
         include_once('dbConnection.php');
         $db = new databaseConnection("BM01");
@@ -83,28 +148,18 @@ class searchEngine {
         $db->queryDatabase("SET CHARACTER SET utf8");
         $db->queryDatabase("SET SESSION collation_connection = 'utf8_unicode_ci'");
         
-        
-        //form query
-        // $query = "
-        //     SELECT * 
-        //     FROM Law_Text 
-        //     WHERE Law_Text.article_text 
-        //     LIKE '%$searchstring%' 
-        //     LIMIT 10";
             
         $query = "
-            SELECT *, Category.category_name AS category, Law_Book.book_title AS law_book
+            SELECT *
             FROM (
                 SELECT *, 
                     MATCH (article_text) 
                     AGAINST ('$searchstring') 
                     as relevance 
                 FROM Law_Text) as derived_table
-            JOIN Category ON derived_table.category_id = Category.id
-            JOIN Law_Book ON derived_table.lawbook_id = Law_Book.id
             WHERE derived_table.relevance > 0
             ORDER BY derived_table.relevance DESC
-            LIMIT 10";
+            LIMIT 100";
             
         
         error_log($query);
@@ -124,12 +179,28 @@ class searchEngine {
             $searchResults['error'] = 'Did not find any resources matching the search string.';
         }
         
+        
+        //summarize the results
+        $source_text = '';
+        foreach ($searchResults['law_articles'] as $article) {
+            $source_text .= "\n\n " . $article['article_text'];
+        }
+        
+        include_once("summarizer.php");
+        
+        $st = new Summarizer();
+        $summary = $st->get_summary($source_text);
+    	$searchResults['summary'] = $summary;
+    	$searchResults['original'] = $source_text;
+        
+        
+        
         //close connection
         $db->closeConnection();
     
-        error_log(json_encode($searchResults));
+        var_dump($st->how_we_did());
     
-        //return article json
+        //return summary json
         return json_encode($searchResults);
     }
     
